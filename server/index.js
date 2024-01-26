1d9b8f66abeea24c733168bd2c36cbc8c704b910
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-01-26 19:17:14 +0530

    Buyer Data Send

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const BuyerData=[
+    {
+        teamName:'MI',
+        currentWallet:23.6,
+        playersBought:7,
+        playerName:'Arshdeep Singh'
+    },{
+        teamName:'CSK',
+        currentWallet:33.6,
+        playersBought:4,
+        playerName:'Bhavya Vohra'
+    },
+    {
+        teamName:'RCB',
+        currentWallet:12.6,
+        playersBought:9,
+        playerName:'Bhaskar Mishra'
+    }
+]
