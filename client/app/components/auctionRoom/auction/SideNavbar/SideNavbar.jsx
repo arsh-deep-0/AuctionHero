863f06667f6721d2bac08bc9070c5fa08184665c
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-01-26 21:54:57 +0530

    Auction Layout-2

--- client/app/components/auctionRoom/auction/SideNavbar/SideNavbar.jsx
+++ client/app/components/auctionRoom/auction/SideNavbar/SideNavbar.jsx
@@ -2,7 +2,7 @@
 
 function SideNavbar() {
   return (
-    <div className="h-full bg-white text-black px-4 py-4">
+    <div className="h-full bg-white text-black px-4 py-4 w-full">
       <h1>Auction Heroes</h1>
     </div>
   )
