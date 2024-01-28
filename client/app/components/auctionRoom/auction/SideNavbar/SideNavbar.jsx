5f066bc45986cfc0cb3e08d8e281db681fa64dde
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-01-28 16:05:11 +0530

    circular progress bar added

--- client/app/components/auctionRoom/auction/SideNavbar/SideNavbar.jsx
+++ client/app/components/auctionRoom/auction/SideNavbar/SideNavbar.jsx
@@ -2,8 +2,8 @@
 
 function SideNavbar() {
   return (
-    <div className="h-full bg-white text-black px-4 py-4 w-full">
-      <h1 className="gabriela font-bold text-xl">Auction Heroes</h1>
+    <div className=" absolute inset-8 bg-white  text-black px-4 py-4 w-1/6  rounded-md ">
+      <h1 className="gabriela font-bold text-xl text-center">Auction Heroes</h1>
     </div>
   )
 }
--- client/app/components/auctionRoom/auction/TeamsWallet/WalletContainer.jsx
+++ client/app/components/auctionRoom/auction/TeamsWallet/WalletContainer.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
-import Buyer from './Buyer'; 
+import Buyer from './Buyer';
 
 function WalletContainer() {
-    const buyerData = [
+    const buyersData = [
         {
             teamName: 'mi',
             currentWallet: 23.6,
+            totalWallet: 50,
             playersBought: 7,
             playerName: 'Arshdeep Singh'
         }, {
             teamName: 'csk',
             currentWallet: 33.6,
+            totalWallet: 50,
             playersBought: 4,
             playerName: 'Bhavya Vohra'
         },
         {
             teamName: 'rcb',
             currentWallet: 12.6,
+            totalWallet: 50,
             playersBought: 9,
             playerName: 'Bhaskar Mishra'
         },
         {
             teamName: 'rr',
             currentWallet: 40.2,
+            totalWallet: 50,
             playersBought: 3,
             playerName: 'Manik Raj'
         },
         {
             teamName: 'kkr',
             currentWallet: 7.7,
+            totalWallet: 50,
             playersBought: 10,
             playerName: 'Anurag Bansal'
         }
@@ -36,9 +41,16 @@ function WalletContainer() {
 
     return (
         <div className="w-full flex flex-col p-2 bg-white gap-2 rounded-md blue-shadow">
-            {buyerData.map((buyer) => (
-                <Buyer key={buyer.teamName} teamName={buyer.teamName} playerName={buyer.playerName}/> 
-            ))}
+            {buyersData.map((buyer) => {
+                const buyerData = {
+                    teamName: buyer.teamName,
+                    currentWallet: buyer.currentWallet,
+                    totalWallet: buyer.totalWallet,
+                    playersBought: buyer.playersBought,
+                    playerName: buyer.playerName
+                }
+                return < Buyer key={buyer.teamName} {...buyerData} />
+            })}
         </div>
     );
 }
