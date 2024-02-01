4ce1a88f5db29ce9077854efae7cbe020b2c3e37
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-02-01 19:31:51 +0530

    backend work started

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
-const BuyerData=[
-    {
-        teamName:'MI',
-        currentWallet:23.6,
-        playersBought:7,
-        playerName:'Arshdeep Singh'
-    },{
-        teamName:'CSK',
-        currentWallet:33.6,
-        playersBought:4,
-        playerName:'Bhavya Vohra'
-    },
-    {
-        teamName:'RCB',
-        currentWallet:12.6,
-        playersBought:9,
-        playerName:'Bhaskar Mishra'
-    }
-]
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const port=process.env.PORT;
+ 
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`);
+});
+
